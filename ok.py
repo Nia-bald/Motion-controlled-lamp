ed75e5b377eb06d29db0c24bcde63b2f92d0518f
Niare Doyom <81701393+Nia-bald@users.noreply.github.com>
2021-12-09 09:06:43 +0530

    Delete ok.py

--- ok.py
+++
@@ -1,9 +0,0 @@
-import serial
-import time
-import cv2
-import handtrackingModule as hm
-import numpy as np
-import math
-arduinoData = serial.Serial('COM3', 9600)
-time.sleep(8)
-arduinoData.write(bytes("X90Y90", 'UTF-8'))
